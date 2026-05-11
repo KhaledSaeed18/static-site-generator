e96c38d3fd217758cad8e6961cc06229873229c4
KhaledSaeed18 <khaled18saeed@gmail.com>
2026-05-12 02:44:31 +0300

    Implement equality comparison for HTMLNode and enhance text_node_to_html_node function for TextNode types

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
-from textnode import TextNode, TextType
+from htmlnode import LeafNode
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -23,6 +24,52 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.LINK, "https://www.boot.dev")
         node2 = TextNode("This is a text node", TextType.LINK, "https://example.com")
         self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("Bold text", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node, LeafNode("b", "Bold text"))
+
+    def test_italic(self):
+        node = TextNode("Italic text", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node, LeafNode("i", "Italic text"))
+
+    def test_code(self):
+        node = TextNode("Code text", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node, LeafNode("code", "Code text"))
+
+    def test_link(self):
+        node = TextNode("anchor text", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(
+            html_node,
+            LeafNode("a", "anchor text", {"href": "https://www.boot.dev"}),
+        )
+
+    def test_image(self):
+        node = TextNode("alt text", TextType.IMAGE, "https://example.com/image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(
+            html_node,
+            LeafNode(
+                "img",
+                "",
+                {"src": "https://example.com/image.png", "alt": "alt text"},
+            ),
+        )
+
+    def test_invalid_type(self):
+        node = TextNode("unknown", "bad type")
+        with self.assertRaises(Exception):
+            text_node_to_html_node(node)
 
 
 if __name__ == "__main__":
