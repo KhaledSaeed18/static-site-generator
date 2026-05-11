11feca2ccbe1ef05583712157c6807a477b57768
KhaledSaeed18 <khaled18saeed@gmail.com>
2026-05-12 02:53:18 +0300

    Add markdown extraction functions for images and links

--- src/test_split.py
+++ src/test_split.py
@@ -1,6 +1,12 @@
 import unittest
 
-from textnode import TextNode, TextType, split_nodes_delimiter
+from textnode import (
+    TextNode,
+    TextType,
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_delimiter,
+)
 
 
 class TestSplitNodesDelimiter(unittest.TestCase):
@@ -33,6 +39,50 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         node = TextNode("This has an `unclosed code", TextType.TEXT)
         with self.assertRaises(Exception):
             split_nodes_delimiter([node], "`", TextType.CODE)
+
+
+class TestMarkdownExtraction(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual(
+            [("image", "https://i.imgur.com/zjjcJKZ.png")],
+            matches,
+        )
+
+    def test_extract_markdown_images_multiple(self):
+        matches = extract_markdown_images(
+            "![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual(
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+            ],
+            matches,
+        )
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev)"
+        )
+        self.assertListEqual(
+            [("to boot dev", "https://www.boot.dev")],
+            matches,
+        )
+
+    def test_extract_markdown_links_multiple(self):
+        matches = extract_markdown_links(
+            "[to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual(
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+            matches,
+        )
 
 
 if __name__ == "__main__":
