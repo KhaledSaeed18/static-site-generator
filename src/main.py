175199aad58e386c2165a2e03e8f61fadd987f48
KhaledSaeed18 <khaled18saeed@gmail.com>
2026-05-12 03:26:51 +0300

    Refactor project structure: remove old HTML and CSS files, add new styles and directory copying functionality

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def copy_directory_contents(source_dir, destination_dir):
+	if os.path.exists(destination_dir):
+		shutil.rmtree(destination_dir)
+
+	os.mkdir(destination_dir)
+
+	for entry in os.listdir(source_dir):
+		source_path = os.path.join(source_dir, entry)
+		destination_path = os.path.join(destination_dir, entry)
+
+		if os.path.isfile(source_path):
+			print(f"Copying file: {source_path} -> {destination_path}")
+			shutil.copy(source_path, destination_path)
+		else:
+			copy_directory_contents(source_path, destination_path)
 
 
 def main():
-	node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-	print(node)
+	copy_directory_contents("static", "public")
 
 
 main()
